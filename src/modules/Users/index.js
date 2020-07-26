90fdfea6d3783be5209a67e241fb4ff88b30ce77
tranthaison1231 <43759215+tranthaison1231@users.noreply.github.com>
2020-07-27 02:33:17 +0700

    Feat/top client (#63)
    * feat(submit): submit challenge
    
    * feat(submit): submit challenge
    
    * feat(submit): submit challenge
    
    * feat(submit): submit challenge
    
    * Develop2 (#43)
    
    * feat(submit): submit challenge
    
    * feat(submit): submit challenge
    
    * feat(submit): submit challenge
    
    * feat(submit): submit challenge
    
    * feat(submit): submit challenge
    
    * feat(submit): submit challenge (#39)
    
    Co-authored-by: thaisonenouvo <son.tran@team.enouvo.com>
    
    * feat(submit): submit challenge
    
    * Develop2 (#40)
    
    * feat(submit): submit challenge
    
    * feat(submit): submit challenge
    
    Co-authored-by: thaisonenouvo <son.tran@team.enouvo.com>
    
    * feat(submit): submit challenge
    
    * Develop2 (#41)
    
    * feat(submit): submit challenge
    
    * feat(submit): submit challenge
    
    * feat(submit): submit challenge
    
    Co-authored-by: thaisonenouvo <son.tran@team.enouvo.com>
    
    * feat(submit): submit challenge
    
    Co-authored-by: thaisonenouvo <son.tran@team.enouvo.com>
    
    * feat(submit): submit challenge
    
    * feat(packge): update package
    
    * feat(export): export excel
    
    * feat(slice): format-crudslice
    
    * fix(conflict): fix conflict
    
    * feat(package): update package
    
    * feat(package): update package
    
    * feat(package): update package
    
    * feat(package): update package
    
    * feat(package): update package
    
    * feat(package): update package
    
    * feat(package): update package
    
    * feat(package): update package
    
    * feat(package): update package
    
    * feat(package): update avatar and package
    
    * feat(package): update avatar and package
    
    * feat(package): update package
    
    * feat(client): top client
    
    * feat(client): top client
    
    * feat: top client
    
    * feat(predict): predict data
    
    * feat(conflict): fix conflict
    
    * feat(theme): add theme
    
    * feat(package): update package
    
    * chore(color): delete file color
    
    * feat(client): top client
    
    * feat(client): top client
    
    * feat(client): top client
    
    * feat(package): update package
    
    * chore(test): add test case for number.js and numberToVietnamese.js
    
    * feat(new): update new
    
    * feat(new): update new
    
    * feat(pacakage): update package
    
    Co-authored-by: thaisonenouvo <son.tran@team.enouvo.com>

--- src/modules/Users/index.js
+++ src/modules/Users/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import RestList from 'components/Rest/RestList';
 import UserTable from 'modules/Users/components/Table';
+import { useSelector } from 'react-redux';
+import { Skeleton } from 'antd';
+import { ROLES } from '../../configs/constants';
+import UserInfo from '../Auth/Info';
 
 const Overview = () => {
-  return <RestList resource="users" table={UserTable} />;
+  const { data: userInfo, loading } = useSelector((state) => state.auth);
+  if (loading === 'getInfo') {
+    return <Skeleton />;
+  }
+  return userInfo.role === ROLES.ADMIN ? (
+    <RestList resource="users" table={UserTable} />
+  ) : (
+    <UserInfo />
+  );
 };
 
 Overview.propTypes = {};
